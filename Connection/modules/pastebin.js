97f8cbace7372cc0144ceeb02014ae23aaed0ec7
MinusGix <MinusGix@Gmail.com>
2018-03-19 00:36:04 -0500

    Removed unncessary logs from Connection pastebin module

--- Connection/modules/pastebin.js
+++ Connection/modules/pastebin.js
@@ -3,10 +3,8 @@ module.exports = {
 	run: async (Data, Client) => {
 		let trigger = 'getpaste ';
 		Client.on('websocket:message:request', async (req, id) => {
-			console.log('pasteybin! ', req);
 			if (req.startsWith(trigger)) {
 				let pasteID = req.substring(trigger.length);
-				console.log(pasteID);
 				Client.respond(id, await Data.Modules.pastebin.getPasteText(pasteID));
 			}
 		});
