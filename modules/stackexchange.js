8b3c84abcab5ae617a4974d2dc2e22ece1039cf0
MinusGix <MinusGix@Gmail.com>
2018-03-19 21:22:12 -0500

    Added a apiURL setting to SE module and added a filter parameter to the getQuestionAnswers SE function. Also fixed where I forgot to call parseSite in parseURL

--- modules/stackexchange.js
+++ modules/stackexchange.js
@@ -3,14 +3,16 @@ module.exports = async (Data) => {
 		name: 'stackexchange',
 		version: '0.0.1',
 		show: true,
+		apiURL: "https://api.stackexchange.com/2.2/",
+
 		run (url) {},
-		async getQuestionAnswers (id, site="stackoverflow") {
-			return (await Data.axios.get(`https://api.stackexchange.com/2.2/questions/8287167/answers?order=desc&sort=activity&site=${site}&filter=!WXiIETACIU-sfj1EnE7ERcE82xydEKEkmRQ*wJm`)).data.items;
+		async getQuestionAnswers (id, site="stackoverflow", filter="!WXiIETACIU-sfj1EnE7ERcE82xydEKEkmRQ*wJm") {
+			return (await Data.axios.get(`${this.apiURL}questions/8287167/answers?order=desc&sort=activity&site=${site}&filter=${filter}`)).data.items;
 		},
 		parseURL (url) {
 			return {
 				id: this.parseID(url),
-				site: this.parseSite || 'stackoverflow'
+				site: this.parseSite(url) || 'stackoverflow'
 			};
 		},
 		parseURLArray (url) { // for easy spreading getQuestionAnswers(...parseURLArray(url))
